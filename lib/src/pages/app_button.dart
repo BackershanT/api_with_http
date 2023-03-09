d78607182c6e4e18fc3f9b849197cd3f950d3ac9
Backershan T <98761199+BackershanT@users.noreply.github.com>
2023-03-09 18:20:56 +0530

    api with http completed get data

--- lib/src/pages/app_button.dart
+++ lib/src/pages/app_button.dart
@@ -1,18 +1,47 @@
 import 'package:flutter/material.dart';
 
 class AppButton extends StatelessWidget {
-  final Function() onPressed;
-  final String title;
-  const AppButton(this.onPressed, this.title, {Key? key}) : super(key: key);
+  final String operation;
+  final String description;
+  final Color? operationColor;
+  final VoidCallback? onPressed;
+
+  const AppButton({
+    Key? key,
+    required this.operation,
+    required this.description,
+    this.operationColor = Colors.black,
+    this.onPressed,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return  Card(
-      child: InkWell(
-        onTap:(){onPressed();} ,
-        child: Container(
-          child: Text(title),
-
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: MaterialButton(
+        onPressed: () {
+          onPressed?.call();
+        },
+        color: Colors.white,
+        height: 60,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              operation,
+              style: TextStyle(
+                fontSize: 32,
+                fontFamily: 'Menlo',
+                fontWeight: FontWeight.w900,
+                color: operationColor,
+              ),
+            ),
+            Text(
+              description,
+              style: const TextStyle(fontSize: 32, fontWeight: FontWeight.w200),
+            ),
+          ],
         ),
       ),
     );
--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,4 +1,6 @@
+import 'package:api_with_http/src/model/user_model.dart';
 import 'package:api_with_http/src/pages/app_button.dart';
+import 'package:api_with_http/src/pages/base_client.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -14,7 +16,62 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       body: Column(
         children: [
+          AppButton(
+            operation: 'Get',
+            description: 'Fetch User',
+            onPressed: () async {
+              var response =
+                  await BaseClient().get('/users').catchError((error) {});
+              if (response == null) return;
+              debugPrint('successful');
+              var users  =userFromJson(response);
+              debugPrint('user count' + users.length.toString());
+            },
+          ),
+          AppButton(
+            operation: 'Post',
+            description: 'Add User',
+            onPressed: () async {
+              var user = User(
+                name: 'backershanT',
+                qualifications:[
+                  Qualification(
+                    completionData: "01/01/2000",degree: 'sslc'
+                  )
+                ]
+              );
+              var response =
+              await BaseClient().post('/users',user).catchError((error) {});
+              if (response == null) return;
+              debugPrint('successful'+user.name.toString());
+            },
+          ),
+          AppButton(
+            operation: 'Put',
+            description: 'Edit User',
+            onPressed: () async {
+              var id =476;
+              var user =User(
+                name: 'Backer',
 
+              );
+              var response =
+              await BaseClient().put('/users/$id',user).catchError((error) {});
+              if (response == null) return;
+              debugPrint('successful'+user.name.toString()+user.id.toString());
+            },
+          ),
+          AppButton(
+            operation: 'Delete',
+            description: 'Delete User',
+            onPressed: () async {
+              var id =479;
+              var response =
+              await BaseClient().delete('/users/$id').catchError((error) {});
+              if (response == null) return;
+              debugPrint('successful+$id');
+            },
+          )
         ],
       ),
     );
